47f5120839e09fc7c5a2e8e1481b6d4233df9b5c
David Conn <dhconn@gmail.com>
2026-07-24 13:26:52 -0400

    Committing two outreach files

--- run-outreach.bat
+++ run-outreach.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\Users\DavidConn\Documents\News Quiz"
+cd /d "C:\Users\dhcon\Projects\News Quiz"
 echo === %DATE% %TIME% === >> outreach-log.txt
 node outreach.js >> outreach-log.txt 2>&1
